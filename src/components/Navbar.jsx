b7c016b7a449f7e6626f03d9e2e6e373a81b53e5
Viqqs <89267977+P-Vikitha@users.noreply.github.com>
2024-07-27 21:23:27 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-start items-center  w-full h-[4rem] bg-[#1b3252] text-white">
       <div className="ml-4">
-        <h2 className="text-3xl ">Wellness Recreats</h2>
+        <h2 className="text-3xl ">Wellness Retreats</h2>
       </div>
     </div>
   );
